cb0e95b49ee5772e53d5ef846ffedd5e5c31668d
Ivan <adamovskij00@mail.ru>
2022-04-09 20:14:11 +0300

    add upd header comp

--- src/app/components/Header/header.component.ts
+++ src/app/components/Header/header.component.ts
@@ -1,20 +1,12 @@
-import {Component} from "@angular/core";
-import {CurrenciesType, DataService} from "../../data.service";
+import {Component, Input} from "@angular/core";
+import {CurrenciesType} from "../../data.service";
 
 @Component({
   selector: 'header',
   templateUrl: 'header.component.html',
   styleUrls: ['header.component.css'],
-  providers: [DataService]
 })
 
 export class HeaderComponent {
-  currencies = [] as Array<CurrenciesType>;
-
-  constructor(private dataService: DataService) {
-  };
-
-  ngOnInit() {
-    this.currencies = this.dataService.getData();
-  }
+  @Input() currencies = [] as Array<CurrenciesType>;
 }
