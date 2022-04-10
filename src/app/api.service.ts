de2a8f8dce3b1944c8c7de6b052484e1f6ad538d
Ivan <adamovskij00@mail.ru>
2022-04-10 16:42:02 +0300

    add upd ApiService

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -3,9 +3,7 @@ import {HttpClient} from '@angular/common/http';
 
 @Injectable()
 export class ApiService {
-
-  constructor(private http: HttpClient) {
-  }
+  constructor(private http: HttpClient) {}
 
   getData() {
     return this.http.get('https://openexchangerates.org/api/latest.json?app_id=537572f59ace483aa2229ffb040427fe')
