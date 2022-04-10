1eeddfecd2c81df6cb119997401b2fe5270f207c
Ivan <adamovskij00@mail.ru>
2022-04-10 16:44:14 +0300

    add upd container component

--- src/app/components/CurrencyExchange/currencyExContainer.component.ts
+++ src/app/components/CurrencyExchange/currencyExContainer.component.ts
@@ -1,4 +1,4 @@
-import {AfterContentChecked, Component} from '@angular/core';
+import {AfterContentChecked, Component, OnInit} from '@angular/core';
 import {CurrenciesType, DataService} from "../../data.service";
 import {ApiService} from "../../api.service";
 
@@ -7,45 +7,54 @@ import {ApiService} from "../../api.service";
   template: `
     <div>
       <currency-comp
+        [currentCurrencyFirst]="currentCurrencyFirst"
+        [currentCurrencySecond]="currentCurrencySecond"
+        (onInput)=" changeCurrencyFieldFirst($event); changeCurrencyFieldSecond($event)"
+        (onChangeF)="changeCurrentCurrencyFirst($event);"
+        (onChangeS)="changeCurrentCurrencySecond($event)"
         (onClick)="changeAction($event)"
-        (onChange)="changeCurrentCurrency($event); changeCurrencyField($event)"
         [isBuying]="isBuying"
         [amountOfUAH]="amountOfUAH"
-        [currencyRate]="currencyRate"
-        [currentCurrency]="currentCurrency"
+        [amountOfUAHSecond]="amountOfUAHSecond"
         [amountOfCurrency]="amountOfCurrency"
+        [amountOfCurrencySecond]="amountOfCurrencySecond"
+        [currencyRate]="currencyRate"
         [currencies]="currencies"
-        [currenciesName]="currenciesName"
       ></currency-comp>
     </div>
   `,
   providers: [DataService, ApiService]
 })
-export class CurrencyExContainerComponent implements AfterContentChecked {
+export class CurrencyExContainerComponent implements OnInit, AfterContentChecked {
   currencies: Array<CurrenciesType> = [];
+  currentCurrencyFirst: string = '';
+  currentCurrencySecond: string = '';
 
   currencyRate: number = 0;
-  currentCurrency: string = 'USD';
   isBuying: boolean = true;
+
   amountOfUAH: string = '';
+  amountOfUAHSecond: string = '';
+
   amountOfCurrency: string = '';
+  amountOfCurrencySecond: string = '';
 
   constructor(private dataService: DataService, private apiService: ApiService) {
   };
 
   currenciesName = this.currencies.map((currency: CurrenciesType) => {
-    if (currency.currencyName === this.currentCurrency) {
+    if (currency.currencyName === this.currentCurrencyFirst) {
       this.currencyRate = this.isBuying ? currency.buyRate : currency.sellRate;
     }
     return currency.currencyName;
   });
 
-  changeCurrencyField (e: any){
+  changeCurrencyFieldFirst(e: any) {
     let value = e.currentTarget.value;
     if (!isFinite(+value)) return;
     if (e.currentTarget.dataset.currency) {
       const trigger: string = e.currentTarget.dataset.currency;
-      if (trigger === 'uah') {
+      if (trigger === 'usd') {
         if (value === '') {
           this.dataService.getChangeCurrencyField(value, value)
         } else {
@@ -61,12 +70,39 @@ export class CurrencyExContainerComponent implements AfterContentChecked {
     }
   };
 
-  changeAction(e: any) {
-    e.currentTarget.dataset.action === 'buy' ? this.dataService.getChangeAction(true) : this.dataService.getChangeAction(false);
+  changeCurrencyFieldSecond(e: any) {
+    let value = e.currentTarget.value;
+    if (!isFinite(+value)) return;
+    if (e.currentTarget.dataset.currency) {
+      const trigger: string = e.currentTarget.dataset.currency;
+      if (trigger === 'usd') {
+        if (value === '') {
+          this.dataService.getChangeCurrencyFieldSecond(value, value)
+        } else {
+          this.dataService.getChangeCurrencyFieldSecond(value, (+Number(value).toFixed(2) / this.currencyRate).toFixed(2));
+        }
+      } else {
+        if (value === '') {
+          this.dataService.getChangeCurrencyFieldSecond(value, value);
+        } else {
+          this.dataService.getChangeCurrencyFieldSecond((+Number(value).toFixed(2) * this.currencyRate).toFixed(2), value);
+        }
+      }
+    }
   };
 
-  changeCurrentCurrency(e: any) {
-    e.currentTarget.ariaLabel && this.dataService.getChangeCurrentCurrency(e.currentTarget.ariaLabel);
+  changeAction(e: any) {
+    e.currentTarget.dataset.action === 'buy'
+      ? this.dataService.getChangeAction(true)
+      : this.dataService.getChangeAction(false);
+  };
+
+  changeCurrentCurrencyFirst(e: any) {
+    e.currentTarget.value && this.dataService.getChangeCurrentCurrencyFirst(e.currentTarget.value);
+  };
+
+  changeCurrentCurrencySecond(e: any) {
+    e.currentTarget.value && this.dataService.getChangeCurrentCurrencySecond(e.currentTarget.value);
   };
 
   ngOnInit() {
@@ -84,12 +120,16 @@ export class CurrencyExContainerComponent implements AfterContentChecked {
   ngAfterContentChecked() {
     this.currencies = this.dataService.getData();
     this.isBuying = this.dataService.isBuying;
-    this.currentCurrency = this.dataService.currentCurrency;
+    this.currentCurrencyFirst = this.dataService.currentCurrencyFirst;
+    this.currentCurrencySecond = this.dataService.currentCurrencySecond;
+
     this.amountOfUAH = this.dataService.amountOfUAH;
+    this.amountOfUAHSecond = this.dataService.amountOfUAHSecond;
     this.amountOfCurrency = this.dataService.amountOfCurrency;
+    this.amountOfCurrencySecond = this.dataService.amountOfCurrencySecond;
 
     this.currenciesName = this.currencies.map((currency: CurrenciesType) => {
-      if (currency.currencyName === this.currentCurrency) {
+      if (currency.currencyName === this.currentCurrencyFirst) {
         this.currencyRate = this.isBuying ? currency.buyRate : currency.sellRate;
       }
       return currency.currencyName;
