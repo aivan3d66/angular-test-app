4ae698f6dd924168ab846c394393e91c461821dd
Ivan <adamovskij00@mail.ru>
2022-04-12 19:18:09 +0300

    add upd app module imports and declarations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,27 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
-import {FormsModule} from "@angular/forms";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {AppComponent} from './app.component';
 import {HeaderComponent} from "./components/Header/header.component";
-import {CurrencyExchangeComponent} from "./components/CurrencyExchange/currencyExchange.component";
-import {CurrencyExContainerComponent} from "./components/CurrencyExchange/currencyExContainer.component";
 import {HttpClientModule} from '@angular/common/http';
+import {Chooser} from "./components/Chooser/chooser.component";
+import {CurrencyExchangeComponent} from './components/CurrencyExchange/currencyExchsnge.component';
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule,
   ],
   declarations: [
     AppComponent,
     HeaderComponent,
-    CurrencyExContainerComponent,
-    CurrencyExchangeComponent
+    CurrencyExchangeComponent,
+    Chooser,
   ],
   bootstrap: [AppComponent]
 })
 export class AppModule {
+
 }
