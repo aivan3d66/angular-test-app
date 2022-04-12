f407cb81638bc226b3170ce1c914cb104221299d
Ivan <adamovskij00@mail.ru>
2022-04-12 19:29:46 +0300

    add url constants

--- src/app/api/api.service.ts
+++ src/app/api/api.service.ts
@@ -1,12 +1,16 @@
 import {Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 
+const BASE_URL = 'https://fixer-fixer-currency-v1.p.rapidapi.com/latest';
+const BASE_CURRENCY = '?base=UAH';
+const CURRENCIES = '&symbols=USD%2CEUR%2CBYN%2CUAH';
+
 @Injectable()
 export class ApiService {
   constructor(private http: HttpClient) {}
 
   getData() {
     const myHeaders = new HttpHeaders().set('X-RapidAPI-Key', 'b128bf2c40msh02bc0681c229b65p17d27ajsnfd4d3f82091e');
-    return this.http.get('https://fixer-fixer-currency-v1.p.rapidapi.com/latest?base=UAH&symbols=USD%2CEUR%2CBYN%2CUAH', {headers: myHeaders})
+    return this.http.get(BASE_URL + BASE_CURRENCY + CURRENCIES, {headers: myHeaders})
   }
 }
